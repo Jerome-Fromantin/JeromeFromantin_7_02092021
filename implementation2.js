67ce5f48266658da74f004e6ba1abde3ca158ca9
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-10-06 15:29:44 +0200

    Suppression des fichiers d'implémentation dans << main >>

--- implementation2.js
+++
@@ -1,6 +0,0 @@
-// Ce fichier contiendra le code nécessaire à la mise en place de la deuxième implémentation
-// de l'algorithme par le biais des propriétés de tableaux telles que "filter".
-
-function triParFiltre() {}
-
-triParFiltre();
